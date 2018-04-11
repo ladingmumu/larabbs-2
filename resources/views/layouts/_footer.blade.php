51bb091794da26bd6ceefb51cb909e7f950e223e
truesnow <chrisx0909@gmail.com>
2018-04-11 15:15:56 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/truesnow" target="_blank">Truesnow</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+
+            <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+        </p>
+    </div>
+</footer>
