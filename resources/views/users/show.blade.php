a0e42dee3ce94778e1bd4d414e30e63f942ddb56
truesnow <chrisx0909@gmail.com>
2018-04-12 12:45:01 +0000

    User profile page

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title', $user->name . '的个人中心')
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="media">
+                    <div align="center">
+                        <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="" class="thumbnail img-responsive" width="300px" height="300px">
+                    </div>
+                    <div class="media-body">
+                        <hr>
+                        <h4><strong>个人简介</strong></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero fuga, cupiditate totam omnis ut debitis accusamus blanditiis recusandae error! Doloremque asperiores deserunt a rerum fugit modi minima voluptate atque, illo!</p>
+                        <hr>
+                        <h4><strong>注册于</strong></h4>
+                        <p>January 01 1901</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <span>
+                    <h1 class="panel-title pull-left" style="font-size:30px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+                </span>
+            </div>
+        </div>
+
+        <hr>
+
+        {{-- 用户发布的内容 --}}
+        <div class="panel panel-default">
+            <div class="panel-body">
+                暂无数据 ~_~
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+@stop
