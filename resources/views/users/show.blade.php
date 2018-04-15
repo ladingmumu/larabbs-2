55b96c2b10eb5e637022a49045d464ddd097db65
truesnow <chrisx0909@gmail.com>
2018-04-15 14:35:21 +0000

    List topics created by user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -19,7 +19,7 @@
                         <p>{{ $user->introduction }}</p>
                         <hr>
                         <h4><strong>注册于</strong></h4>
-                        <p>{{ $user->created_at->diffForHumans() }}</p>
+                        <p>{{ $user->created_at ? $user->created_at->diffForHumans() : '' }}</p>
                     </div>
                 </div>
             </div>
@@ -41,7 +41,15 @@
         {{-- 用户发布的内容 --}}
         <div class="panel panel-default">
             <div class="panel-body">
-                暂无数据 ~_~
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#">Ta 的话题</a></li>
+                            <li><a href="#">Ta 的回复</a></li>
+                        </ul>
+                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    </div>
+                </div>
             </div>
         </div>
     </div>
