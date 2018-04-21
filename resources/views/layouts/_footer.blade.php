36a687c74007896921bc9a4e625db37e858f196b
truesnow <chrisx0909@gmail.com>
2018-04-21 09:13:20 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
         <p class="pull-left">
             由 <a href="https://github.com/truesnow" target="_blank">Truesnow</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
 
-            <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+            <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
         </p>
     </div>
 </footer>
