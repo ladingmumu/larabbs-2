a000c1801713a4a7fe4b55b51f9c5428259ef2a8
truesnow <chrisx0909@gmail.com>
2018-05-17 00:54:24 +0000

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ $api->version('v1', [
             $api->post('topics', 'TopicsController@store')->name('api.topics.store');
             // 修改话题
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
+            $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
         });
     });
 });
